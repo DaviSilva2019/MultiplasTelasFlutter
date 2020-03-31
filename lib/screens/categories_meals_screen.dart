99070852e7d25d7cdc8fb02e7d083ac64f35294e
Davi Silva <davisilva.drummer@gmail.com>
2020-03-30 22:36:02 -0300

    ### Adicionado  - Passagem de dados via construtor

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  
+   final Category category;
+   const CategoriesMealsScreen(this.category); 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(category.title),
+        centerTitle: true,
       ),
       body: Center
         (child: Text('Receitas por categoria'),
