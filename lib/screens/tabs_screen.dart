368dcda811a40fcf81f9c115b18bfa0d0a10f0d0
Davi Silva <davisilva.drummer@gmail.com>
2020-04-16 22:29:34 -0300

    ### Adicionado  - Navegação com tabs na parte inferior da tela

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,37 +2,46 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar?'),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              )
-
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoriteScreen()
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_screens[_selectedScreenIndex]['title']),
+        centerTitle: true,
       ),
-      
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedScreenIndex,
+
+          // type: BottomNavigationBarType.shifting, // com animação. setar background no navigationBarItem
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), title: Text('Categorias')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.star), title: Text('Favoritos')),
+          ]),
     );
   }
 }
